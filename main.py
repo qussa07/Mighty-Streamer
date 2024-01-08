97fb1788aa64b71a44955b109a4f68b35c8b7529
qussa07 <148780123+qussa07@users.noreply.github.com>
2024-01-08 18:26:20 +0300

    доделал загрузку лвла

--- main.py
+++ main.py
@@ -8,6 +8,34 @@ pygame.display.set_caption('Mighty Streamer')
 background = pygame.image.load('images/bg.jpg')
 
 
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+
+tile_images = {
+    'block': pygame.image.load('images/blockDirth.png')
+}
+tile_width = tile_height = 300
+
+def load_level(filename):
+    filename = filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+def generate_level(level, screen):
+    x, y = 0, 0
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '#':
+                screen.blit(tile_images['block'], (tile_width * x, tile_height * y))
+
+    return tiles_group
+
+
 class Player:
     def __init__(self, x, y):
         skin = pygame.image.load('images/Player/PlayerLeft/playerleft1.png')
@@ -116,12 +144,13 @@ class Player:
 
 size = width, height = 1920, 1080
 screen = pygame.display.set_mode(size)
-main_character = Player(60, 780)
+main_character = Player(60, 900)
 frame = 0
 running = True
 while running:
     frame += 1
     screen.blit(background, (0, 0))
+    generate_level(load_level('map.txt'), screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
