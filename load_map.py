cb4a031abce25a08e1f68568ce814a7167cb0c13
qussa07 <148780123+qussa07@users.noreply.github.com>
2024-01-08 17:47:41 +0300

    работаю

---
+++ load_map.py
@@ -0,0 +1,39 @@
+import pygame
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+
+tile_images = {
+    'block': pygame.image.load('images/blockDirth.png')
+}
+tile_width = tile_height = 300
+
+def load_level(filename):
+    filename = filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+def generate_level(level):
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '#':
+                tiles_group.add(tile_images['block'])
+    return tiles_group
+
+
+
+size = WIDTH, HEIGHT = 1920, 1080
+screen = pygame.display.set_mode(size)
+g = generate_level(load_level('map.txt'))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    tiles_group.draw(screen)
+    pygame.display.flip()
