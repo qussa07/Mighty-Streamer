ec94ad62b02aff31684f20a3c643e405a42c5afa
qussa07 <148780123+qussa07@users.noreply.github.com>
2024-01-18 15:20:45 +0300

    дорабатываю

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.display.set_caption('Mighty Streamer')
 background = pygame.image.load('images/bg.jpg')
 
 count = 0
-
+go = 0
 colide = []
 
 all_sprites = pygame.sprite.Group()
@@ -23,6 +23,68 @@ tile_images = {
     'clouds': pygame.image.load('images/blocks/Clouds.png')
 }
 tile_width = tile_height = 70
+
+import pygame
+
+pygame.init()
+bg = pygame.image.load('images/icon.png')
+name = pygame.image.load('images/фыв.png')
+
+
+class Button:
+    def __init__(self, x, y, width, height, text, color, text_color):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.color = color
+        self.text_color = text_color
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        font = pygame.font.SysFont(None, 24)
+        text_img = font.render(self.text, True, self.text_color)
+        text_rect = text_img.get_rect(center=self.rect.center)
+        screen.blit(text_img, text_rect)
+
+    def is_clicked(self, pos):
+        return self.rect.collidepoint(pos)
+
+
+def update(buttons, screen):
+    for i in buttons:
+        i.draw(screen)
+
+
+SCREEN_WIDTH = 392
+SCREEN_HEIGHT = 357
+button0 = Button(-50, -55, 0, 0, '1 Уровень', 'WHITE', 'BLACK')
+button = Button(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2 - 55, 100, 50, '1 Уровень', 'WHITE', 'BLACK')
+button1 = Button(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2, 100, 50, '2 Уровень', 'WHITE', 'BLACK')
+button2 = Button(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2 + 55, 100, 50, '3 Уровень', 'WHITE', 'BLACK')
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+go = 0
+pygame.display.set_icon(bg)
+pygame.display.set_caption('Mighty Streamer')
+buttons = [button0, button, button1, button2]
+running = True
+while running:
+    screen.blit(bg, (0, 0))
+    screen.blit(name, (SCREEN_WIDTH // 2 - 75, 50))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            for i in buttons:
+                if i.rect.collidepoint(event.pos):
+                    go = buttons.index(i)
+
+    if go:
+        pygame.quit()
+        running = False
+    else:
+        update([button, button1, button2], screen)
+        pygame.display.flip()
+
+pygame.quit()
 
 
 def load_level(filename):
