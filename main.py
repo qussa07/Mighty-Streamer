664156dba1be5534ef5910ef5f1e99c73bdb7e41
qussa07 <148780123+qussa07@users.noreply.github.com>
2024-01-18 15:06:48 +0300

    дорабатываю

--- main.py
+++ main.py
@@ -36,98 +36,39 @@ def load_level(filename):
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
+class up:
+    def __init__(self):
+        self.sp = []
+
+    def add(self, a):
+        self.sp.append(a)
+
+    def update(self, player):
+        for i in self.sp:
+            i.mob_view(player)
+
+
+enemy = up()
+
+
 class Mob:
     def __init__(self, x, y):
-        self.base_height = y
-        self.mframe = 0
         skin = pygame.image.load('images/MobSprite/MobLeft/MobLethalLeft1.png')
         self.mob = pygame.sprite.Sprite()
         self.mob.image = skin
-        self.mob.rect = self.mob.image.get_rect()
+        self.mob.rect = self.mob.image.get_rect(center=(x, y))
         self.render = pygame.sprite.Group()
         self.render.add(self.mob)
         self.mob.rect.x = x
         self.mob.rect.y = y
         self.hp = 3
 
-    def attack_left(self, sc, mframe):
-        if mframe == 7:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobAttackLeft/MobAttackLeft1.png')
-
-        elif mframe == 14:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobAttackLeft/MobAttackLeft2.png')
-        elif mframe == 21:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobAttackLeft/MobAttackLeft3.png')
-        elif mframe == 28:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobAttackLeft/MobAttackLeft4.png')
-
-    def attack_right(self, sc, mframe):
-        if mframe == 7:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobAttackRight/MobAttackRight1.png')
-        elif mframe == 14:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobAttackRight/MobAttackRight2.png')
-        elif mframe == 21:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobAttackRight/MobAttackRight3.png')
-        elif mframe == 28:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobAttackRight/MobAttackRight4.png')
-
-    def rendering(self, screen, player):
-        if self.mob.rect.x > player.player.rect.x:
-            self.mob_left(self.mframe)
-        elif self.mob.rect.x < player.player.rect.x:
-            self.mob_right(self.mframe)
-        self.render.draw(screen)
-
-    def mob_left(self, mframe):
-        if mframe == 7:
+    def mob_view(self, player):
+        if self.mob.rect.x > player.x:
             self.mob.image = pygame.image.load(f'images/MobSprite/MobLeft/MobLethalLeft1.png')
-        elif mframe == 14:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobLeft/MobLethalLeft2.png')
-        elif mframe == 21:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobLeft/MobLethalLeft3.png')
-        elif mframe == 28:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobLeft/MobLethalLeft4.png')
-        self.mob.rect.x -= 2
 
-    def mob_right(self, mframe):
-        if mframe == 7:
+        elif self.mob.rect.x < player.x:
             self.mob.image = pygame.image.load(f'images/MobSprite/MobRight/MobLethalRight1.png')
-        elif mframe == 14:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobRight/MobLethalRight2.png')
-        elif mframe == 21:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobRight/MobLethalRight3.png')
-        elif mframe == 28:
-            self.mob.image = pygame.image.load(f'images/MobSprite/MobRight/MobLethalRight3.png')
-        self.mob.rect.x += 2
-
-    def check(self, mframe):
-        if mframe == 28:
-            mframe = 0
-
-        return mframe
-
-    def follow_player(self, player):
-        player_rect = player.get_rect()
-        mob_rect = self.mob.rect
-
-        # Проверяем столкновение
-        if mob_rect.colliderect(player_rect):
-            if self.mob.rect.x > player.player.rect.x:
-                self.attack_left(screen)
-            else:
-                self.attack_right(screen)
-            return
-
-        # Оставляем ваш текущий код для перемещения моба
-        if self.mframe < 28:
-            self.mframe += 1
-        else:
-            self.mframe = 0
-
-        if self.mob.rect.x > player.player.rect.x:
-            self.mob_left(self.mframe)
-        elif self.mob.rect.x < player.player.rect.x:
-            self.mob_right(self.mframe)
 
 
 class Player:
@@ -288,7 +229,7 @@ def generate_level(level, screen, move=0):
                 let_group.add(a)
             elif level[y][x] == '!':
                 a = Mob(tile_width * x + move, tile_height * y - 120)
-                enemy_group.add(a.mob)
+                enemy.add(a)
             elif level[y][x] == '^':
                 screen.blit(
                     tile_images['clouds'].get_rect(topleft=(tile_width * x + move, tile_height * y)))
@@ -297,15 +238,6 @@ def generate_level(level, screen, move=0):
             count = 1
     return d
 
-class up:
-    def __init__(self):
-        self.sp = []
-    def add(self, a):
-        for i in a:
-            self.sp.append(i)
-    def update(self, pla):
-        """for i in self.sp:
-            i.foo"""
 
 size = width, height = 1920, 1080
 screen = pygame.display.set_mode(size)
@@ -337,5 +269,4 @@ while running:
         main_character.walk_left(frame)
     frame = main_character.check(frame)
     clock.tick(fps)
-    enemy_group.draw(screen)
     pygame.display.flip()
