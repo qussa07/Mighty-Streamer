a632f04ae0cb3b72921facacf7451aceaa348446
cvrqs <qqelpnw@gmail.com>
2024-01-31 22:32:37 +0300

    win?

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.font.init()
 
 font = pygame.font.Font(None, 90)
 
-size = width, height = 1600, 900
+size = width, height = 1920, 1080
 screen = pygame.display.set_mode(size)
 running = True
 go = True
@@ -17,11 +17,14 @@ go = True
 while running:
 
     screen.fill('gray')
-    over = font.render('WIN', True, 'purple')
-    screen.blit(over, ((1600 // 2) - 100, 900 // 2))
+    over = font.render('YOU WIN!', True, 'purple')
+    screen.blit(over, ((1920 // 2) - 170, 1080 // 2 - 80))
 
     restart = font.render('Нажмите на любую клавишу для закрытия', True, 'purple')
-    screen.blit(restart, (0, 0))
+    screen.blit(restart, (160, 1080 // 2))
+
+    kill = font.render(f'кол-во убийств - {1}', True, 'purple')
+    screen.blit(kill, (1920 - 900, 100))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
